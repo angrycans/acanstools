49221f9875a689ebf3234a9ba56f24d36d04cb08
acans <angrycans@gmail.com>
2025-02-13 16:06:10 +0800

    comfyui api

---
+++ comfyui/readme.txt
@@ -0,0 +1,2 @@
+http://192.168.2.20:7002/history/b6997b75-1d2c-4128-a05c-2c81fa900950
+http://192.168.2.20:7002/view?filename=latentsync_00004-audio.mp4&subfolder=&type=output
--- soundclone/src/service/home-service.ts
+++ soundclone/src/service/home-service.ts
@@ -6,7 +6,13 @@ class HomeService {
     return new Promise((resolve) => resolve("hello world"));
   }
 
-  test(name) {
+  async test(name) {
+    const dynamic = new Function("modulePath", "return import(modulePath)");
+    const { Client } = await dynamic("@gradio/client");
+    const app = await Client.connect("http://192.168.2.20:7000/", { auth: ["acans", "your_password!"] });
+    const result = await app.predict("/generate_seed", {});
+
+    console.log(result.data);
     return Promise.resolve({
       data: `你好，${name}!`,
     });
