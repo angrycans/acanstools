e1b166d7a3829746182aded9075ed9e6947d9013
acans <angrycans@gmail.com>
2025-02-18 18:19:51 +0800

    add pacakges

--- ai-tools/src/app.config.ts
+++
@@ -1,9 +0,0 @@
-export default defineAppConfig({
-  pages: ["pages/home/index"],
-  window: {
-    backgroundTextStyle: "light",
-    navigationBarBackgroundColor: "#a05aff",
-    navigationBarTitleText: "WeChat",
-    navigationBarTextStyle: "black",
-  },
-});
--- comfyui/readme.txt
+++
@@ -1,2 +0,0 @@
-http://192.168.2.20:7002/history/b6997b75-1d2c-4128-a05c-2c81fa900950
-http://192.168.2.20:7002/view?filename=latentsync_00004-audio.mp4&subfolder=&type=output
